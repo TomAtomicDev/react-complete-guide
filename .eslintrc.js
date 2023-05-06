fdd48f959a7e5f38aa72b5f45308edadea39e4e3
TomAtomicDev <tompenaloza@gmail.com>
2023-05-06 18:21:33 -0400

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: "airbnb",
+  rules: {
+    // enable additional rules
+    indent: ["error", 2],
+    semi: ["error", "always"],
+  },
+};
---
+++ src/__tests__/App.spec.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("App", () => {
+  it("should work as expected", () => {
+    render(<App />);
+    const header = screen.getByRole("heading");
+    expect(header).toBeInTheDocument();
+  });
+});
